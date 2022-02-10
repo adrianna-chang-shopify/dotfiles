16ea4eec46ac48f3b83092c4b479873d2aea79ca
Adrianna Chang <adrianna.chang@shopify.com>
2022-02-10 10:23:37 -0500

    Test 2

--- install.sh
+++ install.sh
@@ -6,13 +6,7 @@ cd "$( cd "$( dirname "$0" )" && pwd )"
 if [ $SPIN ]; then
   echo 'Installing dotfiles for spin...'
 
-  # sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-  # cp -v ~/dotfiles/.zshrc ~/.zshrc
-
-  cat ~/dotfiles/.gitconfig >> ~/.gitconfig
-
-  source $HOME/.zshrc_spin
+  cp -v ~/dotfiles/.gitconfig ~/.gitconfig
 else
   echo "Don't know how to install for this OS or env."
   exit 1
